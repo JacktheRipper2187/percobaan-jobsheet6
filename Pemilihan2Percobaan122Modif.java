7e3c4f83823b47723748386b0304fb287fc775e2
JacktheRipper2187 <ulilf442@gmail.com>
2023-10-21 23:03:42 +0700

    modifikasi kedua

--- Pemilihan2Percobaan122Modif.java
+++ Pemilihan2Percobaan122Modif.java
@@ -10,6 +10,8 @@ public class Pemilihan2Percobaan122Modif {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
+            } else if ((tahun % 400) == 0) {
+                System.out.println("Merupakan tahun");    
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
